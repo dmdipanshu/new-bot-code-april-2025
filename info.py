7050b29a9b706cc62c86457be4293a41471e9c90
dmdipanshu <112119455+dmdipanshu@users.noreply.github.com>
2025-04-05 18:42:20 +0530

    Update info.py

--- info.py
+++ info.py
@@ -37,7 +37,7 @@ LOG_VR_CHANNEL = int(environ.get('LOG_VR_CHANNEL', '0'))
 auth_channel = environ.get('AUTH_CHANNEL', '-1001909436537')
 SUPPORT_GROUP = int(environ.get('SUPPORT_GROUP', '0'))
 request_channel = environ.get('REQUEST_CHANNEL', 'https://t.me/Motivational_videos_4K/4') # If anyone sends a request message to your bot, you will get it in this channel.
-MOVIE_UPDATE_CHANNEL = int(environ.get('MOVIE_UPDATE_CHANNEL', '@filmytak')) # 
+MOVIE_UPDATE_CHANNEL = int(environ.get('MOVIE_UPDATE_CHANNEL', '-1001909436537')) # 
 SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'https://t.me/Motivational_videos_4K/4') #Support group link ( make sure bot is admin )
 #---------------------------------------------------------------
 #---------------------------------------------------------------
