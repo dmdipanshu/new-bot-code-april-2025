1588d1469ae28312e2997b7800780a6d29dbcfc0
dmdipanshu <112119455+dmdipanshu@users.noreply.github.com>
2025-08-01 16:07:25 +0530

    Update info.py

--- info.py
+++ info.py
@@ -41,7 +41,7 @@ MOVIE_UPDATE_CHANNEL = int(environ.get('MOVIE_UPDATE_CHANNEL', '-1001909436537')
 SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'https://t.me/Motivational_videos_4K/4') #Support group link ( make sure bot is admin )
 #---------------------------------------------------------------
 #---------------------------------------------------------------
-IS_VERIFY = is_enabled('IS_VERIFY', True)
+IS_VERIFY = is_enabled('IS_VERIFY', False)
 #---------------------------------------------------------------
 TUTORIAL = environ.get("TUTORIAL", "https://t.me/HOW_TO_OPEN_TGLINKS/21")
 TUTORIAL_2 = environ.get("TUTORIAL_2", "https://t.me/")
